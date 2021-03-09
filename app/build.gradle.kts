df42a2d576c0778d4b38717e427e62060abfcc0a
gilang <gilangabdulaziz@live.com>
2021-03-09 14:01:00 +0700

    add permission module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,18 +49,34 @@ dependencies {
     implementation("com.google.android.material:material:1.3.0")
     implementation("com.android.support.constraint:constraint-layout:2.0.4")
 
+    // dependency injection
     val koinVersion = "1.0.0"
-    implementation("org.koin:koin-android:${koinVersion}")
-    implementation("org.koin:koin-android-scope:${koinVersion}")
+    implementation("org.koin:koin-android:$koinVersion")
+    implementation("org.koin:koin-android-scope:$koinVersion")
 
+    // android nav components
     val navVersion = "2.3.3"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
+    // exo-player
+    val exoVersion = "2.13.2"
+    implementation("com.google.android.exoplayer:exoplayer-core:$exoVersion")
+    implementation("com.google.android.exoplayer:exoplayer-dash:$exoVersion")
+    implementation("com.google.android.exoplayer:exoplayer-hls:$exoVersion")
+    implementation("com.google.android.exoplayer:exoplayer-smoothstreaming:$exoVersion")
+    implementation("com.google.android.exoplayer:exoplayer-ui:$exoVersion")
+
+    // permission library
     implementation("com.karumi:dexter:5.0.0")
+
+    // logging
     implementation("com.jakewharton.timber:timber:4.7.1")
+
+    // ffmpeg
     implementation("com.arthenica:mobile-ffmpeg-full-gpl:4.4.LTS")
 
+    // glide
     val glideVersion = "4.11.0"
     implementation("com.github.bumptech.glide:glide:$glideVersion")
     kapt("com.github.bumptech.glide:compiler:$glideVersion")
@@ -70,7 +86,7 @@ dependencies {
     testImplementation("com.google.truth:truth:1.0.1")
     testImplementation("androidx.test.ext:junit:1.1.2")
     testImplementation("androidx.test.ext:truth:1.3.0")
-    testImplementation("org.mockito:mockito-core:3.0.0")
+    testImplementation("org.mockito:mockito-core:3.1.0")
 
     androidTestImplementation("com.android.support.test:runner:1.0.2")
     androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
